73c9f8ea7450357f1b892d5a1577455519e5884b
marco bonola <marco.bonola@gmail.com>
2017-02-01 13:09:21 +0100

    bug fixed

--- 2face_simulator.py
+++ 2face_simulator.py
@@ -72,6 +72,13 @@ class memA:
 
         return (tmp, self.tot_memory_access_count)
 
+    def query(self, key):
+        ret = self.HT.fullquery(key)
+        self.memory_access_count += ret[4]+1 
+        self.memory_read_count += ret[4]
+        return ret[0]
+
+
     def count(self, key):
         if self.HT.count(key):
             ret = self.HT.fullquery(key)
@@ -123,6 +130,30 @@ class memB:
 
         return (tmp, self.tot_memory_access_count)
 
+    def sync_cache_and_ht(self):
+        for i in self.cache.mem:
+            if i[0]:
+                self.HT.fullinsert(i[0],i[1])
+
+
+    def query(self, key):
+        cache_ret = self.cache.query(key)
+        if cache_ret[0] == "miss":
+            ret = self.HT.fullquery(key)
+            self.memory_access_count += ret[4]
+            self.memory_read_count += ret[4]
+            self.cache.insert(key, ret[0])
+            return ret[0]
+        
+        elif cache_ret[0] == "conflict":
+            #YES -> move new element in cache
+            ht_ret = self.HT.fullquery(key)
+            self.memory_access_count += ht_ret[4]
+            self.memory_read_count += ht_ret[4]
+            self.cache.insert(key, ht_ret[0])
+            return ht_ret[0]
+        else: #cache hit
+            return cache_ret[2]
 
     def count(self, key):
         #check if key in cache
@@ -222,6 +253,9 @@ class memC:
         self.HT =  HTBFmap.HTBFstring(number_of_hash_tables, 2, ht_size, 1000,bf_size)
         #self.BFarray = BFarray(bf_size, number_of_hash_tables)
         self.HT.clear()
+
+    def sync_cache_and_ht(self):
+        return
     
     def sample(self):
         tmp = self.memory_access_count
@@ -254,38 +288,12 @@ class memC:
             #print 'memC:', self.memory_read_count
             return 1
 
-    def load_static_db(self):
-        return
+    def query(self, key):
+        ret =self.HT.fullquery(key)
+        self.memory_access_count += ret[4]+1 
+        self.memory_read_count += ret[4]
 
-    def query_static_db(self):
-        return
-
-    #def count(self, key):
-    #    #compute hash(key)
-
-    #    #bf_ret = (a,b,c,di, ...., numb_of_ht) a=0 if not in first HT, 1 otherwise, etc.... 
-    #    bf_ret = self.BFarray.query(key) 
-
-    #    found = False
-    #    for idx, val in enumerate(bf_ret):
-    #        if val:
-    #            ht_ret = self.HT.direct_query(key, idx)
-    #            self.memory_access_count += 1
-
-    #            if ht_ret[0]==1:
-    #                print 'reciao'
-    #                found = True
-    #                new_val = ht_ret[1]+1
-    #                self.HT.direct_insert(key, new_val, idx, 0, False)
-    #                return new_val
-
-    #    if not found:
-    #        print 'ciao'
-    #        ht_ret = self.HT.fullinsert(key, 1)
-    #        self.memory_access_count += ht_ret[4]
-    #        #self.BFarray.insert(key, ht_ret[1])
-    #        return 1
-
+        return ret[0]
 
     def clear(self):
         self.memory_access_count = 0
@@ -312,6 +320,11 @@ class memD:
         my_cache_size=(ratio*cache_size)/100
         self.cache = Cache(my_cache_size) 
         self.HT.clear()
+
+    def sync_cache_and_ht(self):
+        for i in self.cache.mem:
+            if i[0]:
+                self.HT.fullinsert(i[0],i[1])
 
     def sample(self):
         tmp = self.memory_access_count
@@ -367,6 +380,28 @@ class memD:
             self.cache.insert(key, new_val)
             return new_val
 
+    def query(self, key):
+        cache_ret = self.cache.query(key)
+        
+        if cache_ret[0] == "miss":
+            #check if key in HT
+            ret = self.HT.fullquery(key)
+            self.memory_access_count += ret[4]
+            self.memory_read_count += ret[4]
+            self.cache.insert(key, ret[0])
+            return ret[0] 
+
+        elif cache_ret[0] == "conflict":
+            ht_ret = self.HT.fullquery(key)
+            self.memory_access_count += ht_ret[4]
+            self.memory_read_count += ht_ret[4]
+            self.cache.insert(key, ht_ret[0])
+            return ht_ret[0]
+    
+        else: #cache hit
+            return cache_ret[2]
+
+
     def clear(self):
         self.memory_access_count = 0
         self.HT.clear()
@@ -380,8 +415,8 @@ class memD:
         #print "cache read: %d write: %d"%(self.cache.read_count, self.cache.write_count)
 
 
-if len(sys.argv) != 6:
-    print "usage: 2face_simulator.py <cache_size> <ht_size> <ratio> <trace_file> <key_type> <app_type>"
+if len(sys.argv) != 8:
+    print "usage: 2face_simulator.py <cache_size> <ht_size> <ratio> <trace_file> <key_type> <app_type> <sample_pkt_count>"
     print "where key type can be: \n1: <src_ip>\n2: <dst_ip>\n3: <src_ip, dst_ip>\n4: 5 tuple"
     print "where app type can be: \n0: dynamic\n100: full pre loaded\nx\%: x\% pre loaded " 
     sys.exit()
@@ -390,10 +425,10 @@ cache_size = int(sys.argv[1])
 ht_size = int(sys.argv[2])
 ratio = int(sys.argv[3])
 trace_path = sys.argv[4] 
-sample_period_pkts = 10000
 key_type = int(sys.argv[5])
-
-print "With command line: 2face_simulator.py", sys.argv[1], sys.argv[2], sys.argv[3] 
+app_type = int(sys.argv[6])
+sample_period_pkts = int(sys.argv[7])
+print "With command line: 2face_simulator.py"," ".join(sys.argv[1:])
 
 bf_size = cache_size
 
@@ -415,8 +450,56 @@ elif (ratio==100):
 else:
     testD = memD(ht_size, cache_size, 4, ratio)
 
+if app_type == 0:
+    print "starting dynamic app"
+elif app_type == 100:
+    print "starting static 100% app"
+    #load db
+    while True:
+        l = f.readline()
+        if not l:
+            break
+        fields = l.split("\n")[0].split(" ")
+        if key_type == 1:
+            k = "%s"%(fields[1])
+        elif key_type == 2:
+            k = "%s"%(fields[2])
+        elif key_type == 3:
+            k = "%s %s"%(fields[1],fields[2])
+        elif key_type == 4:
+            k ="%s %s %s %s %s"%(fields[1], fields[2], fields[3], fields[4], fields[5])
+        else:
+            print("wrong key type")
+            sys.exit(-1)
+
+        num_of_packets += 1
+        #faccio count cosi alla fine del while ho il DB pieno
+        a=testA.count(k)
+        d=testD.count(k)
+    
+        if (a!=d):
+            print "ERROR in load"
+            print a,d,k
+            print "HT size is", testA.HT.get_size()
+            print "items in HT:", testA.HT.get_nitem()
+            sys.exit(-1)
+
+
+    
+    #make sure cahce and HT have the same values...
+    testD.sync_cache_and_ht()
+
+    print "processed %d packets. DB is now loaded with load factor %d"%(num_of_packets, 100*testA.HT.get_nitem()/testA.HT.get_size())
+else:
+    print "wrong app type"
+    sys.exit(-1)
+
+print "starting app"
+f.seek(0)
+num_of_packets=0
 while True:
-    if num_of_packets % sample_period_pkts==0:
+
+    if sample_period_pkts and num_of_packets % sample_period_pkts==0:
         testA.mem_report()
         testD.mem_report()
         retA = testA.sample()
@@ -440,19 +523,23 @@ while True:
 
     num_of_packets += 1
 
-    #print k 
-    #print 
-    a=testA.count(k)
-    #b=testB.count(k)
-    #c=testC.count(k)
-    d=testD.count(k)
-    #print b
-    #print c
+    if app_type == 0:
+        a=testA.count(k)
+        d=testD.count(k)
+    elif app_type == 100:
+        a=testA.query(k)
+        d=testD.query(k)
+    else:
+        print "non so come e' possibile perche' ho controllato prima mah oiboh, non costava niente.."
+        sys.exit(-1)
+
     if (a!=d):
-        print "ERROR"
-        print "HT size is", testC.HT.get_size()
-        print "items in HT:", testC.HT.get_nitem()
-        sys.exit()
+        print "ERROR "
+        print a,d,k
+        print "HT size is", testA.HT.get_size()
+        print "items in HT:", testA.HT.get_nitem()
+        sys.exit(-1)
+
 
 print "------------ FINE TRACCIA --------------"
 print "Trace " + tname
